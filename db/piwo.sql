af240dc7c14a888adf428eb3a52a5e4e93256283
Sz4lejesz <122903199+Sz4lejesz@users.noreply.github.com>
2023-03-06 18:23:32 +0100

    Add files via upload

--- db/piwo.sql
+++ db/piwo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 12 Lut 2023, 16:13
+-- Czas generowania: 06 Mar 2023, 18:23
 -- Wersja serwera: 10.4.27-MariaDB
 -- Wersja PHP: 8.1.12
 
@@ -59,8 +59,8 @@ CREATE TABLE `gramatura` (
 --
 
 INSERT INTO `gramatura` (`id`, `name`) VALUES
-(1, 'szt.'),
-(2, 'ml.');
+(1, 'szt'),
+(2, 'ml');
 
 -- --------------------------------------------------------
 
@@ -73,45 +73,72 @@ CREATE TABLE `rekord` (
   `alcohol_id` int(10) NOT NULL,
   `created_on` date NOT NULL DEFAULT current_timestamp(),
   `quantity` int(10) NOT NULL,
-  `gramatura` int(11) NOT NULL
+  `gramatura` int(11) NOT NULL,
+  `user_id` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Zrzut danych tabeli `rekord`
 --
 
-INSERT INTO `rekord` (`id`, `alcohol_id`, `created_on`, `quantity`, `gramatura`) VALUES
-(146, 2, '2020-12-25', 150, 2),
-(147, 3, '2022-09-09', 250, 2),
-(148, 4, '2023-01-09', 500, 2),
-(150, 1, '2022-01-09', 2, 1),
-(151, 1, '2023-01-09', 2, 1),
-(154, 1, '2023-01-09', 3, 1),
-(155, 1, '2023-01-09', 1, 1),
-(158, 1, '2023-01-09', 5, 1),
-(159, 2, '2023-01-09', 50, 2),
-(161, 1, '2023-01-17', 3, 1),
-(162, 1, '2023-01-17', 2, 1),
-(163, 2, '2023-01-17', 200, 2),
-(164, 2, '2023-01-17', 700, 2),
-(165, 1, '2023-02-02', 3, 1),
-(166, 2, '2023-02-02', 200, 2),
-(167, 1, '2023-02-05', 1, 1),
-(168, 2, '2023-02-05', 50, 2),
-(169, 3, '2023-02-05', 330, 2),
-(170, 3, '2023-02-05', 200, 2),
-(171, 5, '2023-02-05', 200, 2),
-(172, 5, '2023-02-05', 150, 2),
-(173, 5, '2023-02-05', 50, 2),
-(174, 5, '2023-02-07', 150, 2),
-(175, 2, '2023-02-07', 25, 2),
-(176, 3, '2023-02-07', 25, 2),
-(177, 4, '2023-02-07', 50, 2),
-(178, 1, '2023-02-07', 3, 1),
-(179, 1, '2023-02-07', 2, 1),
-(181, 1, '2023-03-09', 16, 1),
-(182, 2, '2023-02-12', 250, 2),
-(183, 4, '2023-02-12', 250, 2);
+INSERT INTO `rekord` (`id`, `alcohol_id`, `created_on`, `quantity`, `gramatura`, `user_id`) VALUES
+(146, 2, '2020-12-25', 150, 2, 1),
+(147, 3, '2022-09-09', 250, 2, 0),
+(148, 4, '2023-01-09', 500, 2, 0),
+(150, 1, '2022-01-09', 2, 1, 0),
+(154, 1, '2023-01-09', 3, 1, 2),
+(159, 2, '2023-01-09', 50, 2, 1),
+(164, 2, '2023-01-17', 700, 2, 0),
+(165, 1, '2023-02-02', 3, 1, 0),
+(166, 2, '2023-02-02', 200, 2, 0),
+(167, 1, '2023-02-05', 1, 1, 0),
+(168, 2, '2023-02-05', 50, 2, 0),
+(169, 3, '2023-02-05', 330, 2, 0),
+(170, 3, '2023-02-05', 200, 2, 0),
+(171, 5, '2023-02-05', 200, 2, 0),
+(172, 5, '2023-02-05', 150, 2, 0),
+(173, 5, '2023-02-05', 50, 2, 0),
+(174, 5, '2023-02-07', 150, 2, 0),
+(175, 2, '2023-02-07', 25, 2, 0),
+(176, 3, '2023-02-07', 25, 2, 0),
+(177, 4, '2023-02-07', 50, 2, 0),
+(178, 1, '2023-02-07', 3, 1, 0),
+(179, 1, '2023-02-07', 2, 1, 0),
+(181, 1, '2023-03-09', 16, 1, 0),
+(182, 2, '2023-02-12', 250, 2, 0),
+(183, 4, '2023-02-12', 250, 2, 0),
+(184, 5, '2023-02-12', 250, 2, 0),
+(185, 1, '2023-02-23', 4, 1, 2),
+(186, 5, '2023-02-23', 15, 2, 2),
+(187, 1, '2023-02-23', 4, 1, 1),
+(188, 2, '2023-02-23', 350, 2, 1),
+(189, 3, '2023-02-26', 234, 2, 1),
+(190, 2, '2023-02-26', 500, 2, 1),
+(191, 4, '2023-02-26', 50, 2, 1),
+(192, 5, '2023-02-26', 200, 2, 1),
+(193, 1, '2023-03-05', 2, 1, 1),
+(194, 1, '2023-03-05', 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `user` varchar(20) NOT NULL,
+  `password` varchar(80) NOT NULL,
+  `email` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Zrzut danych tabeli `users`
+--
+
+INSERT INTO `users` (`id`, `user`, `password`, `email`) VALUES
+(1, '123456', '$2y$10$rFhyreKFk9pQtGmmAjSL5.QaoItoQNgAG5rembG9.m..SN9YYyKzy', '123456@gmail.com'),
+(2, '987654', '$2y$10$xrzhtnEhQqXDasw.gbNioOEKnjqFc29B4F7BRf5uIIA2wHos63nEG', '987654@wp.pl');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -136,6 +163,12 @@ ALTER TABLE `rekord`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indeksy dla tabeli `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT dla zrzuconych tabel
 --
 
@@ -155,7 +188,13 @@ ALTER TABLE `gramatura`
 -- AUTO_INCREMENT dla tabeli `rekord`
 --
 ALTER TABLE `rekord`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=184;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=195;
+
+--
+-- AUTO_INCREMENT dla tabeli `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
